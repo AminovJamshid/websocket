6cdd6c5c93c98424f545fb1e950b13ccd2e615b9
Rustam <rustamergashev.sp@gmail.com>
2024-12-05 20:20:28 +0500

    PATCH: Fixed duplicating chats (on backend)

--- app/Actions/HandleNewChat.php
+++ app/Actions/HandleNewChat.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Actions;
 
 use App\Events\GotNewChat;
-use App\Jobs\SendMessage;
 use App\Models\Message;
 use App\Models\Room;
 use Illuminate\Http\Request;
@@ -14,12 +13,27 @@ class HandleNewChat
 {
     public function __invoke(Request $request): ?Room
     {
-        // Create chat
-        $chat = (new CreateChat())($request);
-
-        // Add users to chat
         $senderId   = auth()->id();
         $receiverId = $request->user['id'];
+
+        $sendersRoom   = Room::query()->whereHas('users', function ($query) use ($senderId) {
+            $query->where('user_id', $senderId);
+        });
+        $receiversRoom = Room::query()->whereHas('users', function ($query) use ($receiverId) {
+            $query->where('user_id', $receiverId);
+        });
+
+        $sendersRoomsId   = $sendersRoom->pluck('id')->toArray();
+        $receiversRoomsId = $receiversRoom->pluck('id')->toArray();
+        $roomId           = last(array_intersect($sendersRoomsId, $receiversRoomsId));
+
+        if (!$roomId) {
+            $chat = (new CreateChat())($request);
+        } else {
+            $chat = Room::query()->find($roomId);
+        }
+
+        // Add users to chat
         $chat->users()->sync([$senderId, $receiverId]);
 
         // Chatga va userga messageni bog'lash
